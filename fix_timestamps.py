27b9d935ac7f23abb13ac26a171fb8f72f6b47e9
Steven Grimm <koreth@gmail.com>
2014-08-26 21:08:38 -0700

    Read JSON accelerometer data and set the photo orientation in EXIF

--- fix_timestamps.py
+++ fix_timestamps.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/env python
 import datetime
 import json
 import os
@@ -49,7 +49,7 @@ def GetParsedValidatedArgv(argv):
 def GetJsonPath(jpg_path):
   dirname, jpg = os.path.split(jpg_path)
   image, _ = os.path.splitext(jpg)
-  return os.path.join(dirname, 'meta', image, '.json')
+  return os.path.join(dirname, 'meta', image + '.json')
 
 
 def main(argv):
@@ -60,6 +60,25 @@ def main(argv):
     jpgs = [j for j in filenames if os.path.splitext(j)[1] == '.jpg']
     for jpg in jpgs:
       abspath = os.path.join(dirpath, jpg)
+      jsonpath = GetJsonPath(abspath)
+      orientation = None
+      with open(jsonpath, 'r') as json_file:
+        json_data = json.load(json_file)
+
+        # acc_data.samples has three accelerometer values, one for each
+        # axis. We can't do much with the Z axis, but X and Y can be
+        # used to determine orientation: the axis with the largest absolute
+        # value is closest to vertical, and the rotation depends on whether
+        # the value is positive or negative. This gets turned into a value
+        # from 1-8 per the EXIF "Orientation" field spec.
+        #
+        # TODO: write a metadata field with the actual degrees of rotation
+        #       based on the angle formed by the X and Y values
+        acc_data = json_data['acc_data']['samples'][0]
+        if abs(acc_data[0]) > abs(acc_data[1]):
+          orientation = 1 if acc_data[0] < 0 else 3
+        else:
+          orientation = 8 if acc_data[1] < 0 else 6
 
       dir_timestamp = DIR_TIMESTAMP_RE.search(abspath)
       ts = datetime.datetime(int(dir_timestamp.group('year')),
@@ -74,8 +93,15 @@ def main(argv):
       try:
         metadata.read()
         # TODO(codexile): flag to force even if metadata exists
+        should_write = False
+
+        if 'Exif.Image.Orientation' not in metadata:
+          metadata['Exif.Image.Orientation'] = orientation
+          should_write = True
         if 'Exif.Image.DateTime' not in metadata:
           metadata['Exif.Image.DateTime'] = ts
+          should_write = True
+        if should_write:
           metadata.write()
       except IOError:
         print 'Problem modifying EXIF data for %s.  File corrupt?' % filename
